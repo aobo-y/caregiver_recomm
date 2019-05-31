8e874aafb018fd3d0bc49b9269176a0a97f3f76f
Aobo Yang <yangaobo@gmail.com>
2019-05-30 22:36:01 -0400

    impl scenario

--- src/simulate.py
+++ src/simulate.py
@@ -1,20 +1,34 @@
+import numpy as np
 from alg import LinUCB
 
 class Scenario:
-  def __init__(self, ctx_size, n_choices):
-    pass
+  '''
+  Linear scenario
+  '''
 
-  def newCtx(self):
-    '''
-    Update the ctx and return it
-    '''
-    pass
+  def __init__(self, ctx_size, n_choices, noise=):
+    self.ctx_size = ctx_size
+    self.n_choices = n_choices
+
+    self.weight = np.random.normal(0, 1, (n_choices, ctx_size))
+    self.ctx = None
+
+  def nextCtx(self):
+    ''' Update the ctx and return it '''
+
+    self.ctx = np.random.normal(0, 1, ctx_size)
+    return self.ctx
 
   def reward(self, choice):
-    pass
+    return self.weight[choice] @ self.ctx
 
-  def regret(self, choice):
-    pass
+  def insight(self, choice):
+    ''' Return both the reward & regret '''
+
+    truth = self.weight @ self.ctx
+    opt_reward = truth.max()
+    reward = truth[choice]
+    return reward, opt_reward - reward
 
 class Simulator:
   def __init__(self, scenario):
