2b4586ff0d285fd103b0c1e5d55160ba3ca1dab4
kaiyingshan <shan_kaiying@163.com>
2020-06-07 03:17:23 -0400

    complete config

--- test_scheduled_event.py
+++ test_scheduled_event.py
@@ -13,13 +13,49 @@ one entry of the config list is composed of:
 6. choices to return to server; if multiple children, should match with #5
 """
 
-config = [
-    [0, 600, 10, lambda x: x == 1, [1, 2], [0, 1]],
-    [1, 600, 10, lambda x: x == 1, [2], [1]],
-    [2, 600, 10, lambda x: x == 1, [], [1]],
-]
+config = []
 routes = []
 cur_state = 0
+
+def generate_config():
+    global config
+    morning_msg_suids = [19, 20, 23]
+    evening_msg_suids = [29, ]
+    for i in range(7):
+        # morning message
+        b = i * 12
+        for j in range(3):
+            config.append(
+                [i, 600, 20, lambda x: x['suid'] == morning_msg_suids[j], [b + j + 1], [0, 1]],
+            )
+        # evening message
+        # likert
+        config.append([i, 1380, 20, lambda x: x['suid'] == 29, [b + 4], ['1', '2']])
+        # daily goal
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 5, b + 6], ['1', '2']])
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 7], ['1', '2']])
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 7], ['1', '2']])
+        # ask about recommendation
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 8, b + 10], ['1', '2']]) # stress_manag1
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 9, b + 11], ['1', '2']]) # stress_managyes1
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 11], ['1', '2']]) # stress_managyes2
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 11], ['1', '2']]) # stress_managno1
+
+        config.append([i, 1380, 20, lambda x: x['suid'] == 1, [b + 12], ['1', '2']]) # system_helpful
+
+    base = 85
+    config.append([6, 1380, 40, lambda x: x == 1, [base], ['1', '2']])
+
+    config.append([6, 1380, 40, lambda x: x == 1, [base + 1, base + 2], ['1', '2']])
+    config.append([6, 1380, 40, lambda x: x == 1, [base + 2], [0, 1]])
+
+    config.append([6, 1380, 40, lambda x: x == 1, [base + 3, base + 4], ['1', '2']])
+    config.append([6, 1380, 40, lambda x: x == 1, [base + 4], [0, 1]])
+
+    config.append([6, 1380, 40, lambda x: x == 1, [base + 5, None], ['1', '2']])
+    config.append([6, 1380, 40, lambda x: x == 1, [base + 6], ['1', '2']])
+    config.append([6, 1380, 40, lambda x: x == 1, [], ['1', '2']])
+
 
 def findAllRoutesHelper(graph: List[List[int]], idx: int, prevRoutes: List[List[List[int]]], choice_to_here):
     arr = copy.deepcopy(prevRoutes)
@@ -38,8 +74,17 @@ def findAllRoutesHelper(graph: List[List[int]], idx: int, prevRoutes: List[List[
         return arr
     
     for i in range(len(graph[idx])):
-        temp = findAllRoutesHelper(graph, graph[idx][i], arr, config[idx][5][i])
-        result.extend(temp)
+        if(graph[idx][i] != None):
+            temp = findAllRoutesHelper(graph, graph[idx][i], arr, config[idx][5][i])
+            result.extend(temp)
+        else:
+            route_now = copy.deepcopy(arr)
+            c = config[idx][5][i]
+            for i in range(len(route_now)):
+                size = len(route_now[i])
+                route_now[i][size - 1][1] = c
+            
+            result.extend(route_now)
 
     return result
 
@@ -53,6 +98,7 @@ def prepare_test():
     global routes
     # format: [(index_in_config, response), (index_in_config, response), ..
     #          (index_in_config, response), ...]
+    # [0, 1, 2] [0, 2]
     routes = findAllRoutes()
 
     #verify routes
